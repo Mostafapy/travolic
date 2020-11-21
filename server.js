db96c9600015596034ba90dbde69166eee922c5e
Mostafa Hisham <mmham3919@gmail.com>
2020-11-21 09:38:33 +0200

    adding server.js

---
+++ server.js
@@ -0,0 +1,50 @@
+// Load env vars
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const logger = require('./utils/logger')('Server');
+
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(
+    morgan((tokens, req, res) =>
+      [
+        `<${process.env.NODE_ENV}>`,
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'),
+        '-',
+        tokens['response-time'](req, res),
+        'ms',
+      ].join(' ')
+    )
+  );
+}
+
+// Port
+const port = process.env.PORT || '3000';
+
+// Listen
+const server = app.listen(port, () => 
+  logger.log(`App Listen Successfully To Port ${port}`)
+);
+
+// Unhandled Promise Rejection Handler
+process.on('unhandledRejection', (ex) => {
+  logger.error(`${ex.message}`.red, ex);
+  app.use((_req, res) => {
+    res.status(500).json({
+      success: false,
+      msg: '500 Internet Error',
+      data: null,
+    });
+  });
+
+  // eslint-disable-next-line implicit-arrow-linebreak
+  server.close(() => process.exit(1));
+});
