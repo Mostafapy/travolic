125a3c027dfb40db139320c1fbaae3fac70f3c9e
Mostafa Hisham <mmham3919@gmail.com>
2020-11-21 20:04:16 +0200

    adding dataFormat helper function

---
+++ helpers/dateFormat.js
@@ -0,0 +1,9 @@
+const format = (inputDate) => {
+    var date = new Date(inputDate);
+    if (!isNaN(date.getTime())) {
+        // Months use 0 index.
+        return date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear();
+    }
+}
+
+module.exports = format;
